ded9ea903a1bde613987fe41eeaa4f3273b49aa1
diamondc78 <diamantina.cortes@outlook.com>
2025-06-17 18:22:21 -0400

    Add files via upload

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=5)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.title
+    
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=255)
+    No_of_guest = models.IntegerField(max_length=6)
+    bookingdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
